00cc710d1dd4183e5c6b7e9b4994f69f67d3a53f
ultimatefree21-lang <ultimatefree21@gmail.com>
2025-12-02 23:38:48 +0200

    Add PrayerTime data class with Room Entity annotations

---
+++ src/main/java/com/ultimatefree21/prayertimes/model/PrayerTime.kt
@@ -0,0 +1,14 @@
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "prayer_times")
+data class PrayerTime(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val date: String,
+    val fajr: String,
+    val dhuhr: String,
+    val asr: String,
+    val maghrib: String,
+    val isha: String
+)
